2b5db619c84983435b72cb9296ddb423daa9fa2b
Wouter Bruggeman <wouterbruggeman@live.nl>
2018-04-23 14:46:59 +0000

    Fix lock script

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 i3lock -B 8 -e \
-	--textcolor=00000000 \
+	--verifcolor=00000000 \
+	--wrongcolor=00000000 \
 	--insidecolor=00000000 --insidevercolor=00000000 \
 	--insidewrongcolor=00000000 \
 	--ringcolor=00000000 --ringvercolor=FFFFFF80 \
@@ -8,8 +9,8 @@ i3lock -B 8 -e \
 	--keyhlcolor=FFFFFF80 --bshlcolor=CF262380 \
 	--linecolor=00000000 --separatorcolor=00000000 \
 	-k \
-	--timefont="sans-serif" --datefont="sans-serif" \
 	--timesize=60 --datesize=18 \
 	--timestr="%H:%M" --datestr="%A, %B %e" \
-	--timepos="ix-200:iy-250" --datepos="ix-200:iy-215" \
-	--timecolor=FFFFFFC0 --datecolor=FFFFFF80
+	--timepos="ix:iy-250" --datepos="ix:iy-215" \
+	--timecolor=FFFFFFC0 --datecolor=FFFFFF80 \
+	--time-font="sans-serif" --date-font="sans-serif" \
