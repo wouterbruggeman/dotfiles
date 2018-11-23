ecf6c8349e2ae637722e70c3706d31f0a6493622
Wouter Bruggeman <wouterbruggeman@live.nl>
2018-11-23 17:40:58 +0100

    Add display setup script

--- .scripts/fixDisplaySetup.sh
+++ .scripts/fixDisplaySetup.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 
-xrandrString="`xrandr | grep "HDMI-0 disconnected"`"
+# Kill intel-virtual-o if it's already running
+pkill intel-virtual-o
+# Start intel-virtual-output
+intel-virtual-output
 
+#Check if there is an monitor connected
+xrandrString="`xrandr | grep "VIRTUAL1 disconnected"`"
 if [ "${xrandrString}" == "" ]; then
 	# There is an external display connected
-	xrandr --output HDMI-0 --auto --right-of eDP-1-1
+	# Apply the home screenlayout and reload the g810-led profile
+	sh ~/.screenlayout/home.sh
+	g810-led -p ~/.config/g810-led/profile
 else
-	# There is no external display connected
-	xrandr --auto
+	# There is no external display connected, apply the no_hdmi layout
+	sh ~/.screenlayout/no_hdmi.sh
 fi
 
+# Always make sure the background looks good.
 feh --no-fehbg --bg-fill ~/Images/bg.png &
