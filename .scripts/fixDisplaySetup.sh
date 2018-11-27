30a78b4063974ae17ab5b33e58b1b888450a88c2
Wouter Bruggeman <wouterbruggeman@live.nl>
2018-11-27 08:01:33 +0100

    Change display script

--- .scripts/fixDisplaySetup.sh
+++ .scripts/fixDisplaySetup.sh
@@ -3,7 +3,7 @@
 # Kill intel-virtual-o if it's already running
 pkill intel-virtual-o
 # Start intel-virtual-output
-intel-virtual-output
+optirun intel-virtual-output
 
 # Check if there is a monitor connected
 xrandrString="`xrandr | grep "VIRTUAL1 disconnected"`"
@@ -15,6 +15,9 @@ if [ "${xrandrString}" == "" ]; then
 else
 	# There is no external display connected, apply the no_hdmi layout
 	sh ~/.screenlayout/no_hdmi.sh
+
+	# Kill the intel virtual output process. It's not needed anymore.
+	pkill intel-virtual-o
 fi
 
 # Always make sure the background looks good.
