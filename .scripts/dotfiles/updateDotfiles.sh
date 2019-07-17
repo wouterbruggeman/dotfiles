f4c8cef336628f9110f81e8a762331b74849124a
Wouter Bruggeman <wouterbruggeman@live.nl>
2019-07-17 20:46:16 +0200

    Add xorg config files and add an alias

--- .scripts/dotfiles/updateDotfiles.sh
+++ .scripts/dotfiles/updateDotfiles.sh
@@ -32,6 +32,7 @@ cp /etc/mkinitcpio.conf $DOT_DIR/etc/mkinitcpio.conf
 cp /etc/fstab $DOT_DIR/etc/fstab
 cp /etc/iptables/iptables.rules $DOT_DIR/etc/iptables/iptables.rules
 cp -r /etc/modprobe.d $DOT_DIR/etc/
+cp -r /etc/X11/xorg.conf.d $DOT_DIR/etc/
 
 #Copy all background images to the dotfiles directory
 cp -r /home/wouter/media/backgrounds $DOT_DIR/media
